e42df17bc6d90c94ee6d88621d45acfd56464e59
Amzzy78 <murphy.annmarie78@gmail.com>
2021-10-01 15:25:32 +0000

    Add answers

--- run.py
+++ run.py
@@ -19,3 +19,10 @@ questions = {
     "In Irish Folklore what was eaten eaten by the Salmon, fished up by the druid, and cooked by young Finn, who, as sorcerer’s apprentice, burns his thumb on the Salmon’s skin, sticks thumb in mouth, and attains all the wisdom in his master’s stead?: ": "C",
     "In Irish Mythology what is the name of the story of the son of a warrior chieftain, who experiences an ‘Isle of intoxicating wine fruits’ during his journey to avenge his father’s death?: ": "A",
 }
+
+options = [["A. Druids", "B. Fionn mac Cumhaill", "C. Michael D Higgins", "D. Biddy Early"]
+          ["A. Poitin", "B. Mead", "C. Guinness", "D. Whiskey"]
+          ["A. Algae", "B. Hazelnuts", "C. Seaweed", "D. Potatoes"]
+          ["A. The Voyage of Máel Dúin", "B. Tír na nÓg", "C. The Children of Lir", "D. Táin Bó Cúailnge"]
+
+]
